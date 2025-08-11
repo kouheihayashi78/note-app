0daeff32524ef25f297d4a1c05e05203ddbc6ca6
kouhei <kouhei.basketball7.8@gmail.com>
2025-08-11 14:56:55 +0900

    add: ノートのコンポーネントをざっくり実装

---
+++ src/app/components/NoteItem.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import React from "react";
+
+const NoteItem = () => {
+  return (
+    <div className="bg-gray-100 rounded-lg p-5 relative">
+      <Link href="/notes" className="absolute -top-4 left-4">
+        <span className="w-8 h-8 inline-flex justify-center items-center bg-purple-500 hover:bg-purple-700 text-white rounded-full">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-5 h-5"
+            viewBox="0 0 25 25"
+            fill="currentColor"
+          >
+            <path d="M7.127 22.562l-7.127 1.438 1.438-7.128 5.689 5.69zm1.414-1.414l11.228-11.225-5.69-5.692-11.227 11.227 5.689 5.69zm9.768-21.148l-2.816 2.817 5.691 5.691 2.816-2.819-5.691-5.689z" />
+          </svg>
+        </span>
+      </Link>
+      <Link href="/notes">
+        <h3 className="text-purple-500 hover:text-purple-700 text-lg md:text-xl font-semibold mb-3 underline">
+          ダミーテキスト
+        </h3>
+      </Link>
+    </div>
+  );
+};
+
+export default NoteItem;
