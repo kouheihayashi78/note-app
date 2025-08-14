b9415c9cffb507de521e0d9cf08a730ceeeda6b3
kouhei <kouhei.basketball7.8@gmail.com>
2025-08-14 14:59:57 +0900

    add: 静的エクスポート時のダイナミックルートに対する必須の指定を追加

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import Note from "./Note";
-import { getNotesDetail } from "@/lib/client";
+import { getNotesDetail, getNotesList } from "@/lib/client";
 
-export default async function page({ params }: { params: { id:string }  }) {
-    const id = await params.id
+export default async function page({ params }: { params: Promise<{ id: string }>; }) {
+    // paramsの中からidを抽出する
+    const { id } = await params;
     const note = await getNotesDetail(id);
     return (
         <main className="mx-2 sm:mx-4">
@@ -12,4 +13,11 @@ export default async function page({ params }: { params: { id:string }  }) {
             <Note key={note.id} note={note}/>
         </main>
     );
+}
+
+// 静的エクスポート時ではダイナミックルートに対してgenerateStaticParams指定必須
+export async function generateStaticParams() {
+    const {contents} = await getNotesList();
+    // idのプロパティにしているが、ここはフォルダーの名前に合わせる
+    return contents.map((n) => ({ id: n.id }));
 }
